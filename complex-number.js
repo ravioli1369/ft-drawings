1538bbc706b838cfbcbdcb870859baf2e7a481ee
Ravi Kumar <81402844+ravioli1369@users.noreply.github.com>
2023-12-13 06:49:56 +0530

    discrete fourier transform, changes to make import statement work

--- complex-number.js
+++ complex-number.js
@@ -14,50 +14,91 @@
  * @see https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/complex-number
  */
 
-class ComplexNumber {
-  constructor(real, imaginary) {
-    this.real = real;
-    this.imaginary = imaginary;
-  }
+export default class ComplexNumber {
+    constructor(real, imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
 
-  add(other) {
-    return new ComplexNumber(
-      this.real + other.real,
-      this.imaginary + other.imaginary
-    );
-  }
+    add(other) {
+        return new ComplexNumber(
+            this.real + other.real,
+            this.imaginary + other.imaginary
+        );
+    }
 
-  subtract(other) {
-    return new ComplexNumber(
-      this.real - other.real,
-      this.imaginary - other.imaginary
-    );
-  }
+    Add(one, two) {
+        return new ComplexNumber(
+            one.real + two.real,
+            one.imaginary + two.imaginary
+        );
+    }
 
-  multiply(other) {
-    const real = this.real * other.real - this.imaginary * other.imaginary;
-    const imaginary = this.real * other.imaginary + this.imaginary * other.real;
-    return new ComplexNumber(real, imaginary);
-  }
+    subtract(other) {
+        return new ComplexNumber(
+            this.real - other.real,
+            this.imaginary - other.imaginary
+        );
+    }
 
-  magnitude() {
-    return Math.sqrt(this.real ** 2 + this.imaginary ** 2);
-  }
+    Subtract(one, two) {
+        return new ComplexNumber(
+            one.real - two.real,
+            one.imaginary - two.imaginary
+        );
+    }
 
-  phase() {
-    return Math.atan2(this.imaginary, this.real);
-  }
+    multiply(other) {
+        const real = this.real * other.real - this.imaginary * other.imaginary;
+        const imaginary =
+            this.real * other.imaginary + this.imaginary * other.real;
+        return new ComplexNumber(real, imaginary);
+    }
 
-  toPolar() {
-    return {
-      r: this.magnitude(),
-      phi: this.phase(),
-    };
-  }
+    Multiply(one, two) {
+        const real = one.real * two.real - one.imaginary * two.imaginary;
+        const imaginary = one.real * two.imaginary + one.imaginary * two.real;
+        return new ComplexNumber(real, imaginary);
+    }
 
-  toString() {
-    return `${this.real} + ${this.imaginary}i`;
-  }
+    divide(other) {
+        const denominator = other.real ** 2 + other.imaginary ** 2;
+        const real =
+            (this.real * other.real + this.imaginary * other.imaginary) /
+            denominator;
+        const imaginary =
+            (this.imaginary * other.real - this.real * other.imaginary) /
+            denominator;
+        return new ComplexNumber(real, imaginary);
+    }
+
+    Divide(one, two) {
+        const denominator = two.real ** 2 + two.imaginary ** 2;
+        const real =
+            (one.real * two.real + one.imaginary * two.imaginary) / denominator;
+        const imaginary =
+            (one.imaginary * two.real - one.real * two.imaginary) / denominator;
+        return new ComplexNumber(real, imaginary);
+    }
+
+    magnitude() {
+        return Math.sqrt(this.real ** 2 + this.imaginary ** 2);
+    }
+
+    phase() {
+        return Math.atan2(this.imaginary, this.real);
+    }
+
+    toPolar() {
+        return {
+            r: this.magnitude(),
+            phi: this.phase(),
+        };
+    }
+
+    toString() {
+        return `${this.real} + ${this.imaginary}i`;
+    }
 }
 
 // Usage example:
---
+++ discrete-fourier-transform.js
@@ -0,0 +1,39 @@
+/**
+ * Discrete Fourier transform (DFT)
+ * @class discreteFourierTransform
+ * @param {number[]} input
+ * @return {ComplexNumber[]}
+ * @see https://en.wikipedia.org/wiki/Discrete_Fourier_transform
+ * @see https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/fourier-transform
+ */
+
+import ComplexNumber from "./complex-number.js";
+
+function discreteFourierTransform(input) {
+    const N = input.length;
+    const output = [];
+
+    for (let k = 0; k < N; k++) {
+        let sum = new ComplexNumber(0, 0);
+
+        for (let n = 0; n < N; n++) {
+            const phi = (2 * Math.PI * k * n) / N;
+            const wn = new ComplexNumber(Math.cos(phi), -Math.sin(phi));
+            const xn = new ComplexNumber(input[n], 0);
+            const product = wn.multiply(xn);
+            sum = sum.add(product);
+        }
+
+        output.push(sum);
+    }
+
+    return output;
+}
+
+// Usage example
+// const input = [1, 2, 3, 4];
+// const result = discreteFourierTransform(input);
+
+// for (const res of result) {
+//     console.log(res.toString());
+// }
